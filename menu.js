94730380c86ccd34cd39dba6c394751c289f4a58
MaBokSing <karweng381@gmail.com>
2024-11-14 08:30:52 +0800

    Add few pages

--- menu.js
+++ menu.js
@@ -16,8 +16,17 @@ document.getElementById("menu").outerHTML = `
         <div id="menu_home" class="menuSlot active" onclick="window.location.href = 'main.html';"">
             <img src="png/home.png" style="width: 50px;" />
         </div>
-        <div id="menu_transaction" class="menuSlot active" onclick="window.location.href = 'transaction.html';">
+        <div id="menu_history" class="menuSlot active" onclick="window.location.href = 'transaction_history.html';">
             <img src="png/refund.png" style="width: 50px;" />
+        </div>
+        <div id="menu_manage" class="menuSlot active" onclick="window.location.href = 'manage_inventory.html';">
+            <img src="png/manage_inventory.png" style="width: 50px;" />
+        </div>
+        <div id="menu_modify" class="menuSlot active" onclick="window.location.href = 'modify_inventory.html';">
+            <img src="png/modify_inventory.png" style="width: 50px;" />
+        </div>
+        <div id="menu_report" class="menuSlot active" onclick="window.location.href = 'report.html';">
+            <img src="png/report.png" style="width: 50px;" />
         </div>
         <div style="position: relative; height: min(60vh, 20vh);"></div>
         <div id="menu_logout" class="menuSlot active" onclick="logout()">
@@ -29,6 +38,10 @@ document.getElementById("menu").outerHTML = `
 
 var page = document.getElementById("Title").innerHTML.toLowerCase();
 if (page == "order") { page = "home" };
+if (page == "transaction history") { page = "history" };
+if (page == "modify inventory") { page = "modify" };
+if (page == "manage inventory") { page = "manage" };
+if (page == "sales report") { page = "report" };
 
 document.getElementById("menu_" + page).className = "menuSlot now";
 
