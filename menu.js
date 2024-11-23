d91e477e45f0f2898d431002e5381b08d5abd708
MaBokSing <karweng381@gmail.com>
2024-11-23 20:55:46 +0800

    Update staff code CSS, Improve transaction history table CSS

--- menu.js
+++ menu.js
@@ -4,15 +4,6 @@ document.getElementById("menu").outerHTML = `
 <div id="menu"
     style="grid-area: menu; width: 70px; height: 100vh; background-image: linear-gradient(rgb(240, 240, 240), lightgray); position: relative; transform: translate(-50%, -50%); top: 50%; left: 50%;">
     <div style="position: relative; transform: translateY(-50%); top: 50%;">
-        <div class="menuSlot active">
-            <div style="height: 6px;"></div>
-            <div style="height: 8px; background-color: rgb(54, 54, 54); width: 50px;"></div>
-            <div style="height: 10px;"></div>
-            <div style="height: 8px; background-color: rgb(54, 54, 54); width: 50px;"></div>
-            <div style="height: 10px;"></div>
-            <div style="height: 8px; background-color: rgb(54, 54, 54); width: 50px;"></div>
-            <div style="height: 6px;"></div>
-        </div>
         <div id="menu_home" class="menuSlot active" onclick="window.location.href = 'main.html';"">
             <img src="png/home.png" style="width: 50px;" />
         </div>
