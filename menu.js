6024a079401d7703d35e827249a953999d68db61
MaBokSing <karweng381@gmail.com>
2024-11-24 17:57:44 +0800

    Upload Logo and improve layout

--- menu.js
+++ menu.js
@@ -1,8 +1,7 @@
 // To make menubar UI consistent
 
 document.getElementById("menu").outerHTML = `
-<div id="menu"
-    style="grid-area: menu; width: 70px; height: 100vh; background-image: linear-gradient(rgb(240, 240, 240), lightgray); position: relative; transform: translate(-50%, -50%); top: 50%; left: 50%;">
+<div id="menu" class="menu">
     <div style="position: relative; transform: translateY(-50%); top: 50%;">
         <div id="menu_home" class="menuSlot active" onclick="window.location.href = 'main.html';"">
             <img src="png/home.png" style="width: 50px;" />
